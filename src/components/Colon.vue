ff035a5a22db201794a71e7278ef93679e97faf4
Bronson Naylor <bronsonnaylor@gmail.com>
2020-05-28 22:39:01 -0600

    Added blinking functionality for Colon.

--- src/components/Colon.vue
+++ src/components/Colon.vue
@@ -1,12 +1,34 @@
 <template>
     <span>
-        <img class="colon" src="../assets/Colon-off.png">
+        <img class="colon" :src="image">
     </span>
 </template>
 
 <script>
 export default {
   name: 'Colon',
+  data: function() {
+      return {
+          image: require("@/assets/Colon-off.png"),
+          toggle: true
+      };
+  },
+  methods: {
+      colonBlink: function () {
+          setInterval(()=>{
+              if (this.toggle) {
+                this.image = require("@/assets/Colon-on.png")
+                this.toggle = false
+                return
+              }
+              this.image = require("@/assets/Colon-off.png")
+              this.toggle = true
+          }, 1000)
+      }
+  },
+  mounted: function () {
+      this.colonBlink()
+  }
 }
 </script>
 
