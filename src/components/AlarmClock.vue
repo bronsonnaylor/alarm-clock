aad3b14af458dc1c543491616da43219c9ca2ce9
Bronson Naylor <bronsonnaylor@gmail.com>
2020-06-04 00:27:27 -0600

    Added the ability to cycle digits on AlarmClock 0-9.

--- src/components/AlarmClock.vue
+++ src/components/AlarmClock.vue
@@ -2,32 +2,32 @@
     <div>
         <div class="clock">
             <span class="section">
-                <img class="arrow" src="../assets/Arrow-Up.png"/>
-                <Meridian v-bind:meridian="AM"/>
+                <img v-on:click="meridianSwitch" class="arrow" src="../assets/Arrow-Up.png"/>
+                <Meridian v-bind:meridian="true" ref="meridian"/>
                 <img class="arrow" src="../assets/Arrow-Down.png"/>
             </span>
             <span class="section">
-                <img class="arrow" src="../assets/Arrow-Up.png"/>
-                <Digit v-bind:timeDigit="0"/>
-                <img class="arrow" src="../assets/Arrow-Down.png"/>
+                <img v-on:click="digitSwitchUp(0)" class="arrow" src="../assets/Arrow-Up.png"/>
+                <Digit v-bind:timeDigit="0" ref="digit0"/>
+                <img v-on:click="digitSwitchDown(0)" class="arrow" src="../assets/Arrow-Down.png"/>
             </span>
             <span class="section">
-                <img class="arrow" src="../assets/Arrow-Up.png"/>
-                <Digit v-bind:timeDigit="0"/>
-                <img class="arrow" src="../assets/Arrow-Down.png"/>
+                <img v-on:click="digitSwitchUp(1)" class="arrow" src="../assets/Arrow-Up.png"/>
+                <Digit v-bind:timeDigit="0" ref="digit1"/>
+                <img v-on:click="digitSwitchDown(1)" class="arrow" src="../assets/Arrow-Down.png"/>
             </span>
             <span class="section">
                 <Colon/>
             </span>
             <span class="section">
-                <img class="arrow" src="../assets/Arrow-Up.png"/>
-                <Digit v-bind:timeDigit="0"/>
-                <img class="arrow" src="../assets/Arrow-Down.png"/>
+                <img v-on:click="digitSwitchUp(2)" class="arrow" src="../assets/Arrow-Up.png"/>
+                <Digit v-bind:timeDigit="0" ref="digit2"/>
+                <img v-on:click="digitSwitchDown(2)" class="arrow" src="../assets/Arrow-Down.png"/>
             </span>
             <span class="section">
-                <img class="arrow" src="../assets/Arrow-Up.png"/>
-                <Digit v-bind:timeDigit="0"/>
-                <img class="arrow" src="../assets/Arrow-Down.png"/>
+                <img v-on:click="digitSwitchUp(3)" class="arrow" src="../assets/Arrow-Up.png"/>
+                <Digit v-bind:timeDigit="0" ref="digit3"/>
+                <img v-on:click="digitSwitchDown(3)" class="arrow" src="../assets/Arrow-Down.png"/>
             </span>
         </div>
         <div>
@@ -49,7 +49,25 @@ export default {
     },
     data: function() {
         return {
-            AM: true
+            AMPM: true,
+            digit: [0, 0, 0, 0]
+        }
+    },
+    methods: {
+        meridianSwitch: function () {
+            this.AMPM = !this.AMPM
+            this.$refs.meridian.defineMeridian(this.AMPM)
+        },
+        digitSwitchUp: function (refNum) {
+            this.digit[refNum] = (this.digit[refNum] + 1) % 10
+            this.$refs[`digit${refNum}`].defineDigit(this.digit[refNum])
+        },
+        digitSwitchDown: function (refNum) {
+            this.digit[refNum]--
+            if (this.digit[refNum] < 0) {
+                this.digit[refNum] = 9
+            }
+            this.$refs[`digit${refNum}`].defineDigit(this.digit[refNum])
         }
     }
 }
