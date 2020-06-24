eedf2bf18887b57901529d547f7c1f7540ec5b37
Bronson Naylor <bronsonnaylor@gmail.com>
2020-06-23 23:03:00 -0600

    Modified digitSwitch function to use fewer conditionals.

--- src/components/AlarmClock.vue
+++ src/components/AlarmClock.vue
@@ -9,12 +9,12 @@
             <span class="section">
                 <img v-on:click="digitSwitch(0, 1)" class="arrow" src="../assets/Arrow-Up.png"/>
                 <Digit v-bind:timeDigit="0" ref="digit0"/>
-                <img v-on:click="digitSwitch(0, 0)" class="arrow" src="../assets/Arrow-Down.png"/>
+                <img v-on:click="digitSwitch(0, -1)" class="arrow" src="../assets/Arrow-Down.png"/>
             </span>
             <span class="section">
                 <img v-on:click="digitSwitch(1, 1)" class="arrow" src="../assets/Arrow-Up.png"/>
                 <Digit v-bind:timeDigit="0" ref="digit1"/>
-                <img v-on:click="digitSwitch(1, 0)" class="arrow" src="../assets/Arrow-Down.png"/>
+                <img v-on:click="digitSwitch(1, -1)" class="arrow" src="../assets/Arrow-Down.png"/>
             </span>
             <span class="section">
                 <Colon/>
@@ -22,12 +22,12 @@
             <span class="section">
                 <img v-on:click="digitSwitch(2, 1)" class="arrow" src="../assets/Arrow-Up.png"/>
                 <Digit v-bind:timeDigit="0" ref="digit2"/>
-                <img v-on:click="digitSwitch(2, 0)" class="arrow" src="../assets/Arrow-Down.png"/>
+                <img v-on:click="digitSwitch(2, -1)" class="arrow" src="../assets/Arrow-Down.png"/>
             </span>
             <span class="section">
                 <img v-on:click="digitSwitch(3, 1)" class="arrow" src="../assets/Arrow-Up.png"/>
                 <Digit v-bind:timeDigit="0" ref="digit3"/>
-                <img v-on:click="digitSwitch(3, 0)" class="arrow" src="../assets/Arrow-Down.png"/>
+                <img v-on:click="digitSwitch(3, -1)" class="arrow" src="../assets/Arrow-Down.png"/>
             </span>
         </div>
         <div class="buttons">
@@ -67,20 +67,13 @@ export default {
             this.$emit('time-changed')
         },
         digitSwitch: function (refNum, direction) {
-            if (direction === 1) {
-                this.digit[refNum] = (this.digit[refNum] + 1) % this.limit[refNum]
-                this.$refs[`digit${refNum}`].defineDigit(this.digit[refNum])
-                this.$emit('time-changed')
+            this.digit[refNum] += direction
+            if (this.digit[refNum] < 0) {
+                this.digit[refNum] = this.limit[refNum] - 1
             }
-            if (direction === 0) {
-                if (this.digit[refNum] === 0) {
-                    this.digit[refNum] = this.limit[refNum] - 1
-                } else {
-                    this.digit[refNum]--
-                }
-                this.$refs[`digit${refNum}`].defineDigit(this.digit[refNum])
-                this.$emit('time-changed')
-            }
+            this.digit[refNum] = this.digit[refNum] % this.limit[refNum]
+            this.$refs[`digit${refNum}`].defineDigit(this.digit[refNum])
+            this.$emit('time-changed')
         },
         getTime: function () {
             return `${this.AMPM}${this.digit[0]}${this.digit[1]}${this.digit[2]}${this.digit[3]}`
